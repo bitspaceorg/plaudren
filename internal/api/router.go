b6986abaf61924b4c1d42df0274b02ca8fbdfcf9
SuryaPrakash <surya0prakash@proton.me>
2025-01-11 23:03:02 +0530

    [Feat] Router Refactor
    - changed the register
    - added route type

--- internal/api/router.go
+++ internal/api/router.go
@@ -2,41 +2,129 @@ package api
 
 import (
 	"encoding/json"
-	"log/slog"
+	"fmt"
 	"net/http"
 )
 
-type Router struct {
-	path   string
-	router *http.ServeMux
-}
-
+// type of the function that handles the http request
 type HTTPFunc func(http.ResponseWriter, *http.Request) *ApiError
 
-func NewRouter(path string) *Router {
-	return &Router{
-		path:   path,
-		router: &http.ServeMux{},
+type HTTPMethod string
+
+// type of the router which registers the functions
+type HTTPRouter interface {
+
+	// http methods
+	// the param func should implement the HTTPFunc interface
+	Get(string, HTTPFunc)
+	Post(string, HTTPFunc)
+	Put(string, HTTPFunc)
+	Patch(string, HTTPFunc)
+	Delete(string, HTTPFunc)
+
+	// returns all the routes
+	GetRoutes() []Route
+
+	//register another router with the current router
+	Handle(string, HTTPRouter)
+
+	//register the router with a mux to handle http transport
+	Register(*http.ServeMux)
+}
+
+// allowed methods
+const (
+	GET    HTTPMethod = "GET"
+	POST   HTTPMethod = "POST"
+	PUT    HTTPMethod = "PUT"
+	PATCH  HTTPMethod = "PATCH"
+	DELETE HTTPMethod = "DELETE"
+)
+
+/*
+TODO:
+1. fix strip '/' in url
+2. test for nested routers
+2. test for other methods
+*/
+
+
+//handles the URI of the api which is to be registered with the Router
+type Route struct {
+	method   HTTPMethod
+	path     string
+	httpfunc HTTPFunc
+}
+
+func (route *Route) GetRoute() string {
+	return fmt.Sprintf("%s %s", route.method, route.path)
+}
+
+// return the http handler for the routes
+// handles the encoding (json,grpc...)
+func (route *Route) GetHandler() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := route.httpfunc(w, r); err != nil {
+			json.NewEncoder(w).Encode(err)
+			return
+		}
 	}
 }
 
-func (r *Router) GetPath() string {
-	return r.path
+func NewRoute(method HTTPMethod, path string, httpfunc HTTPFunc) *Route {
+	return &Route{
+		method:   method,
+		path:     path,
+		httpfunc: httpfunc,
+	}
 }
 
-func (r *Router) GetHandler() http.Handler {
-	return r.router
+// router contains a group of routes
+// should implement the HTTPRouter interface
+type Router struct {
+	path   string
+	routes []Route
+}
+
+func NewRouter(path string) *Router {
+	return &Router{
+		path: path,
+	}
 }
 
 func (r *Router) Get(path string, httpFunc HTTPFunc) {
-	r.router.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("type", "application/json")
-		if err := httpFunc(w, r); err != nil {
-			w.WriteHeader(err.code)
-			err := json.NewEncoder(w).Encode(err)
-			if err != nil {
-				slog.Error(err.Error())
-			}
-		}
-	})
+	r.routes = append(r.routes, *NewRoute(GET, path, httpFunc))
+}
+
+func (r *Router) Put(path string, httpFunc HTTPFunc) {
+	r.routes = append(r.routes, *NewRoute(PUT, path, httpFunc))
+}
+
+func (r *Router) Post(path string, httpFunc HTTPFunc) {
+	r.routes = append(r.routes, *NewRoute(POST, path, httpFunc))
+}
+
+func (r *Router) Patch(path string, httpFunc HTTPFunc) {
+	r.routes = append(r.routes, *NewRoute(PATCH, path, httpFunc))
+}
+
+func (r *Router) Delete(path string, httpFunc HTTPFunc) {
+	r.routes = append(r.routes, *NewRoute(DELETE, path, httpFunc))
+}
+
+func (r *Router) GetRoutes() []Route {
+	return r.routes
+}
+
+func (r *Router) Handle(path string, router HTTPRouter) {
+	for _, route := range router.GetRoutes() {
+		route.path = fmt.Sprintf("%s/%s", r.path, route.path)
+		r.routes = append(r.routes, route)
+	}
+}
+
+func (r *Router) Register(mux *http.ServeMux) {
+	for _, route := range r.routes {
+		mux.HandleFunc(route.GetRoute(), route.GetHandler())
+	}
 }
